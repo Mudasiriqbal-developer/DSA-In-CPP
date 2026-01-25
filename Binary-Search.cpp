07971fa9915b1d715a313736c7e79a7d584c45ca
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-01-25 20:54:12 +0500

    style: format code for consistency and readability in Binary-Search.cpp

--- Binary-Search.cpp
+++ Binary-Search.cpp
@@ -1,26 +1,33 @@
-#include<iostream>
-#include<vector>
-using namespace std;  
+#include <iostream>
+#include <vector>
+using namespace std;
 
-int binarySearch(vector<int>& arr, int target) {
+int binarySearch(vector<int> &arr, int target)
+{
   int st = 0, end = arr.size() - 1;
 
-  while(st <= end) {
-    int mid = st + (end - st)/2;
-    if(target > arr[mid]) {
+  while (st <= end)
+  {
+    int mid = st + (end - st) / 2;
+    if (target > arr[mid])
+    {
       st = mid + 1;
-    } else if(target < arr[mid]) {
+    }
+    else if (target < arr[mid])
+    {
       st = mid - 1;
-    } else {
+    }
+    else
+    {
       return mid;
     }
   }
-  cout<<"Noting Found"<<endl;
+  cout << "Noting Found" << endl;
 }
 
-int main()  
-{  
-	vector<int> arr = {-1,0,3,5,9,12,15};
+int main()
+{
+  vector<int> arr = {-1, 0, 3, 5, 9, 12, 15};
   vector<int> arr2 = {1};
   int tar = 1;
   cout << binarySearch(arr2, tar);
